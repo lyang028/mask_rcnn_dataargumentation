478d0800664ecc6620f9aa96fafdf84c4e3d0d63
liqun yang <lyang028@fiu.edu>
2019-12-14 20:08:36 -0500

    test

--- workers_keep_train.py
+++ workers_keep_train.py
@@ -26,7 +26,7 @@ Usage: import the module (see Jupyter notebooks for examples), or run from
     # Apply color splash to video using the last weights you trained
     python3 worker.py splash --weights=last --video=<URL or path to file>
 """
-
+# test
 import os
 import sys
 import random
